a2044cbe4597ca598f9329e21e65bd329f87f0f1
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-31 23:07:34 +0500

    🔥 Day 1 finished and gcdOfStrings done

--- Day1/index.js
+++ Day1/index.js
@@ -16,4 +16,17 @@ function mergeAlternately(word1, word2) {
     return newStr;
 }
 // ----------------------------------------------------------------
+function gcdOfStrings(str1, str2) {
+    if (str1 + str2 != str2 + str1)
+        return "";
+    let max = Math.max(str1.length, str2.length);
+    while (max > 0) {
+        if (str1.length % max === 0 && str2.length % max === 0) {
+            return str1.substring(0, max);
+        }
+        max -= 1;
+    }
+    return '';
+}
+//-------------------------- END ---------------------------
 //# sourceMappingURL=index.js.map
--- Day1/index.ts
+++ Day1/index.ts
@@ -20,3 +20,17 @@ function mergeAlternately(word1: string, word2: string): string {
 }
 
 // ----------------------------------------------------------------
+
+function gcdOfStrings(str1: string, str2: string): string {
+    if (str1 + str2 != str2 + str1) return "";
+    let max = Math.max(str1.length, str2.length);
+    while (max > 0) {
+        if (str1.length % max === 0 && str2.length % max === 0) {
+            return str1.substring(0, max);
+        }
+        max -= 1;
+    }
+    return '';
+}
+
+//-------------------------- END ---------------------------
