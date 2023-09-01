545c4b352f9fad7b2b8390061cba785387b26d3e
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-01 21:52:32 +0500

    ✅ Day 2 finished and canPlaceFlowers done

--- Day2/index.js
+++ Day2/index.js
@@ -7,4 +7,17 @@ function kidsWithCandies(candies, extraCandies) {
     return candies.map((item) => item + extraCandies >= maxCand);
 }
 //---------------------------------------------------
+function canPlaceFlowers(flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+            flowerbed[i] = 1;
+            count++;
+        }
+    }
+    return count >= n;
+}
+//---------------------- END ----------------
 //# sourceMappingURL=index.js.map
--- Day2/index.ts
+++ Day2/index.ts
@@ -9,3 +9,20 @@ function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
 }
 
 //---------------------------------------------------
+
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    let count: number = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (
+            flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)
+        ) {
+            flowerbed[i] = 1;
+            count++;
+        }
+    }
+    return count >= n;
+}
+
+//---------------------- END ----------------
