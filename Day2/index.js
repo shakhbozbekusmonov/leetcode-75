f11d8c512b4ac8fb08e52d07d9824be5cef1a5ba
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-01 21:38:37 +0500

    ✅ kidsWithCandies done

--- Day2/index.js
+++ Day2/index.js
@@ -2,4 +2,9 @@
 // LeetCode 75 Challenge started by Miracle Programmer Shakhbozbek Usmonov.
 // Day 2 started by Shakhbozbek Usmonov
 //---------------- Start ----------------
+function kidsWithCandies(candies, extraCandies) {
+    let maxCand = Math.max(...candies);
+    return candies.map((item) => item + extraCandies >= maxCand);
+}
+//---------------------------------------------------
 //# sourceMappingURL=index.js.map
--- Day2/index.ts
+++ Day2/index.ts
@@ -5,15 +5,7 @@
 
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
     let maxCand: number = Math.max(...candies);
-    let result: boolean[] = [];
-    for (let i = 0; i < candies.length; i++) {
-        if (maxCand <= extraCandies + candies[i]) {
-            result.push(true);
-        } else {
-            result.push(false);
-        }
-    }
-    return result;
+    return candies.map((item) => item + extraCandies >= maxCand);
 }
 
 //---------------------------------------------------
