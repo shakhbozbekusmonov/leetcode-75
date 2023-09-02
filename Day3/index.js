9b46c963e291abab824088cf2eaac4ea817ec417
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-02 22:14:42 +0500

    ✅ Day 3 finished and 238. Product of Array Except Self  done

--- Day3/index.js
+++ Day3/index.js
@@ -46,4 +46,18 @@ function reverseWords(s) {
     return newStr.reverse().join("");
 }
 //----------------------------------------------------------------
+function productExceptSelf(nums) {
+    const result = new Array(nums.length).fill(1);
+    let prefix = 1;
+    let postfix = 1;
+    for (let i = 0; i < nums.length; ++i) {
+        result[i] *= prefix;
+        result[nums.length - 1 - i] *= postfix;
+        prefix *= nums[i];
+        postfix *= nums[nums.length - 1 - i];
+    }
+    return result;
+}
+;
+//------------------- END ------------------------------------
 //# sourceMappingURL=index.js.map
--- Day3/index.ts
+++ Day3/index.ts
@@ -48,3 +48,18 @@ function reverseWords(s: string): string {
 }
 
 //----------------------------------------------------------------
+
+function productExceptSelf(nums: number[]): number[] {
+    const result = new Array(nums.length).fill(1);
+    let prefix = 1;
+    let postfix = 1;
+    for (let i = 0; i < nums.length; ++i) {
+        result[i] *= prefix
+        result[nums.length - 1 - i] *= postfix;
+        prefix *= nums[i]
+        postfix *= nums[nums.length - 1 - i];
+    }
+    return result;
+};
+
+//------------------- END ------------------------------------
