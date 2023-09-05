da9760bade73da772b9713fad3ebfcbb77c3f574
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-05 16:31:26 +0500

    ✅ Day 5 finished and findMaxAverage done

--- Day5/index.js
+++ Day5/index.js
@@ -49,4 +49,17 @@ function maxOperations(nums, k) {
     return operationCount;
 }
 //---------------------------------------------------------------------
+function findMaxAverage(nums, k) {
+    let sum = 0, max = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    max = sum / k;
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        max = Math.max(max, sum / k);
+    }
+    return max;
+}
+//------------------------ END -------------------------
 //# sourceMappingURL=index.js.map
--- Day5/index.ts
+++ Day5/index.ts
@@ -62,3 +62,23 @@ function maxOperations(nums: number[], k: number): number {
 }
 
 //---------------------------------------------------------------------
+
+function findMaxAverage(nums: number[], k: number): number {
+    let sum: number = 0,
+        max: number = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    max = sum / k;
+
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        max = Math.max(max, sum / k);
+    }
+
+    return max;
+}
+
+//------------------------ END -------------------------
